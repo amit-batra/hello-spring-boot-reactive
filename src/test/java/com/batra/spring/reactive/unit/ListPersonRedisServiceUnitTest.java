1e81c78067b33ce15cd9b19958c96e4d67b8e539
Amit Batra <abatra@adobe.com>
2022-05-09 01:31:08 +0530

    Added a blank Unit Test class for ListPersonRedisService class.

---
+++ src/test/java/com/batra/spring/reactive/unit/ListPersonRedisServiceUnitTest.java
@@ -0,0 +1,41 @@
+package com.batra.spring.reactive.unit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListPersonRedisServiceUnitTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void testClearList() {
+	}
+
+	@Test
+	void testSize() {
+	}
+
+	@Test
+	void testGetList() {
+	}
+
+	@Test
+	void testAddPersonToLeft() {
+	}
+
+	@Test
+	void testAddPersonToRight() {
+	}
+
+	@Test
+	void testRemovePersonFromLeft() {
+	}
+
+	@Test
+	void testRemovePersonFromRight() {
+	}
+}
