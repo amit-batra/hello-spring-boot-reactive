97092fa0998e9d20fb169845bcd4e19bd37c799a
Amit Batra <abatra@adobe.com>
2022-05-10 20:50:02 +0530

    Completed the ListPersonRedisService unit tests.

--- src/test/java/com/batra/spring/reactive/unit/ListPersonRedisServiceUnitTest.java
+++ src/test/java/com/batra/spring/reactive/unit/ListPersonRedisServiceUnitTest.java
@@ -1,15 +1,28 @@
 package com.batra.spring.reactive.unit;
 
+import com.batra.spring.reactive.entity.Person;
 import com.batra.spring.reactive.service.ListPersonRedisService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ListPersonRedisServiceUnitTest {
+
+	private static final String LIST_KEY = "peopleList";
+	private static final List<Person> LIST_PERSON = List.of(
+		new Person("Sumit Mohan", 25),
+		new Person("Mohit Mohan", 35),
+		new Person("Namit Mohan", 45)
+	);
 
 	private final ListPersonRedisService service;
 
@@ -20,6 +33,10 @@ class ListPersonRedisServiceUnitTest {
 
 	@BeforeEach
 	public void setUp() {
+		this.service.clearList(LIST_KEY);
+		LIST_PERSON.forEach(person ->
+			this.service.addPersonToRight(LIST_KEY, person)
+		);
 	}
 
 	@Test
@@ -29,29 +46,82 @@ class ListPersonRedisServiceUnitTest {
 
 	@Test
 	public void testClearList() {
+		this.service.clearList(LIST_KEY);
+
+		final List<Person> expectedList = List.of();
+		final List<Person> actualList = this.service.getList(LIST_KEY);
+
+		assertEquals(expectedList, actualList);
 	}
 
 	@Test
 	public void testSize() {
+		final long expectedSize = LIST_PERSON.size();
+		final long actualSize = this.service.size(LIST_KEY);
+
+		assertEquals(expectedSize, actualSize);
 	}
 
 	@Test
 	public void testGetList() {
+		final List<Person> expectedList = LIST_PERSON;
+		final List<Person> actualList = this.service.getList(LIST_KEY);
+
+		assertEquals(expectedList, actualList);
 	}
 
 	@Test
 	public void testAddPersonToLeft() {
+		final Person person = new Person("Kirit Mohan", 15);
+		this.service.addPersonToLeft(LIST_KEY, person);
+
+		final List<Person> expectedList = new LinkedList<>();
+		expectedList.add(person);
+		expectedList.addAll(LIST_PERSON);
+
+		final List<Person> actualList = this.service.getList(LIST_KEY);
+		assertEquals(expectedList, actualList);
 	}
 
 	@Test
 	public void testAddPersonToRight() {
+		final Person person = new Person("Bhagat Mohan", 55);
+		this.service.addPersonToRight(LIST_KEY, person);
+
+		final List<Person> expectedList = new LinkedList<>(LIST_PERSON);
+		expectedList.add(person);
+
+		final List<Person> actualList = this.service.getList(LIST_KEY);
+		assertEquals(expectedList, actualList);
 	}
 
 	@Test
 	public void testRemovePersonFromLeft() {
+
+		final Long initialSize = Long.valueOf(LIST_PERSON.size());
+		final Person expectedPerson = LIST_PERSON.get(0);
+
+		final Person removedPerson = this.service.removePersonFromLeft(LIST_KEY);
+		final Long remainingSize = this.service.size(LIST_KEY);
+
+		assertAll(
+			() -> assertEquals(initialSize - 1, remainingSize),
+			() -> assertEquals(expectedPerson, removedPerson)
+		);
 	}
 
 	@Test
 	public void testRemovePersonFromRight() {
+
+		final Long initialSize = Long.valueOf(LIST_PERSON.size());
+		final Person expectedPerson = LIST_PERSON.get(LIST_PERSON.size() - 1);
+
+		final Person removedPerson = this.service.removePersonFromRight(LIST_KEY);
+		final Long remainingSize = this.service.size(LIST_KEY);
+
+		assertAll(
+			() -> assertEquals(initialSize - 1, remainingSize),
+			() -> assertEquals(expectedPerson, removedPerson)
+		);
 	}
 }
