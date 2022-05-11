7352a077e76bd9fcc3e4989d746b8724a3085d51
Amit Batra <abatra@adobe.com>
2022-05-11 17:20:14 +0530

    Added implementation of testAddPerson() to HashPersonRedisServiceUnitTests.

--- src/test/java/com/batra/spring/reactive/unit/HashPersonRedisServiceUnitTests.java
+++ src/test/java/com/batra/spring/reactive/unit/HashPersonRedisServiceUnitTests.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -62,6 +66,31 @@ public class HashPersonRedisServiceUnitTests {
 	public void testContainsPerson() {
 		PERSON_MAP.keySet().forEach(key ->
 			assertTrue(this.service.containsPerson(MAP_REDIS_KEY, key))
+		);
+	}
+
+	/**
+	 * Validates the <code>addPerson</code> method in class
+	 * {@link HashPersonRedisService}. It does that by comparing
+	 * the before and after state of the Redis hash.
+	 */
+	@Test
+	public void testAddPerson() {
+
+		final List<Person> beforeList = this.service.getPeople(MAP_REDIS_KEY);
+		final Long beforeSize = (long) beforeList.size();
+
+		final String personKey = "lalit";
+		final Person person = new Person("Lalit Mohan", 55);
+		this.service.addPerson(MAP_REDIS_KEY, personKey, person);
+
+		final List<Person> afterList = this.service.getPeople(MAP_REDIS_KEY);
+		final Long afterSize = this.service.size(MAP_REDIS_KEY);
+
+		assertAll(
+			() -> assertEquals(beforeSize + 1, afterSize),
+			() -> assertFalse(beforeList.contains(person)),
+			() -> assertTrue(afterList.contains(person))
 		);
 	}
 }
